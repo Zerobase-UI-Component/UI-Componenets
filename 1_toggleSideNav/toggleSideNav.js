8c5f6a7132410814a0d1494f9c8536a6567a458a
Seungyeon Lee <sie9412@gmail.com>
2021-11-29 11:44:06 +0900

    add 2_scrollingGoToTop

--- 1_toggleSideNav/toggleSideNav.js
+++ 1_toggleSideNav/toggleSideNav.js
@@ -1,1 +1,17 @@
-alert("hello")
+// DOMs
+const $toggleBtn = document.querySelector('.toggle');
+const $nav = document.querySelector('nav');
+
+// Event handlers
+const toggleClassName = () => {
+  console.log($nav.classList)
+  const isActive = [...$nav.classList].includes('active');
+  if (isActive) {
+    $nav.classList.remove('active');
+  } else {
+    $nav.classList.add('active');
+  }
+}
+
+// Event Handlers 등록
+$toggleBtn.addEventListener('click', toggleClassName);
---
+++ 2_scrollingGotoTop/scrollingGotoTop.js
@@ -0,0 +1,28 @@
+const $scrollIcon = document.querySelector('.scroll-icon');
+
+const throttle = (callback, delay) => {
+  let timeId;
+
+  return (event) => {
+    if (timeId) return;
+    timeId = setTimeout(() => {
+      callback(event);
+      timeId = null;
+    }, delay, event);
+  }
+}
+
+const goToTop = () => {
+  window.scrollTo(0, 0);
+}
+
+window.addEventListener('scroll', throttle(() => {
+  console.log(window.pageYOffset)
+  if (window.pageYOffset < 100) {
+    $scrollIcon.style.display = 'none';
+  } else if (window.pageYOffset >= 100) {
+    $scrollIcon.style.display = 'block';
+  }
+}, 100))
+
+$scrollIcon.addEventListener('click', goToTop);
