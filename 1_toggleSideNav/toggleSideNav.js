8f73806ae090dac2a2c3993a866bebeac44b7e77
jnhro1 <jnhro1@gmail.com>
2021-12-02 18:57:54 +0900

    refactoring 1

--- 1_toggleSideNav/toggleSideNav.js
+++ 1_toggleSideNav/toggleSideNav.js
@@ -1,18 +1,4 @@
-// DOMs
-const $toggleBtn = document.querySelector('.toggle');
-const $nav = document.querySelector('nav');
-
-// Event handlers
-const toggleClassName = ($Element, className) => {
-  const isActive = [...$Element.classList].includes(className);
-  if (isActive) {
-    $Element.classList.remove(className);
-  } else {
-    $Element.classList.add(className);
-  }
-};
-
 // Event Handlers 등록
-$toggleBtn.addEventListener('click', () => {
-  toggleClassName($nav, 'active');
+document.querySelector('.toggle').addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('active');
 });
